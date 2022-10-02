44c7addcb7bc27beae95c3bd7bc10677249a13e4
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 13:49:50 +0200

    :sparkles: Add Order Benchmarks

---
+++ src/Performance.NET7.Benchmarks/LINQ/Order/OrderBenchmarks.cs
@@ -0,0 +1,31 @@
+﻿namespace Performance.NET7.Benchmarks.LINQ.Order;
+
+[RPlotExporter]
+[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0", baseline: true)]
+[MemoryDiagnoser(displayGenColumns: false)]
+[HideColumns(Column.Job, Column.StdDev, Column.Error, Column.RatioSD, Column.AllocRatio)]
+public class OrderBenchmarks
+{
+    [Params(1000)]
+    public int Length { get; set; }
+
+    private double[] arr;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var random = new Random();
+        arr = Enumerable
+            .Range(0, Length)
+            .Select(_ => random.NextDouble())
+            .ToArray();
+    }
+
+    [Benchmark]
+    public double[] OrderBy() => arr.OrderBy(d => d).ToArray();
+
+#if NET7_0
+    [Benchmark]
+    public double[] Order() => arr.Order().ToArray();
+#endif
+}
