442eca245e66ece575096e89f78f2c537aff52e9
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 13:14:51 +0200

    :construction: Adapt to new Template

--- src/Performance.NET7.Benchmarks/LINQ/MinMax/MinMaxBenchmarks.cs
+++ src/Performance.NET7.Benchmarks/LINQ/MinMax/MinMaxBenchmarks.cs
@@ -4,9 +4,9 @@ namespace Performance.NET7.Benchmarks.LINQ.MinMax;
 
 [RPlotExporter]
 [SimpleJob(RuntimeMoniker.Net60, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 6.0")]
-[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0")]
+[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0", baseline: true)]
 [MemoryDiagnoser(displayGenColumns: false)]
-[HideColumns(Column.Job, Column.StdDev, Column.Error)]
+[HideColumns(Column.Job, Column.StdDev, Column.Error, Column.RatioSD, Column.AllocRatio)]
 public class MinMaxBenchmarks
 {
     [Params(1000)]
