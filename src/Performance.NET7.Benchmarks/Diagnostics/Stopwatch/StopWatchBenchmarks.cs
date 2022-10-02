d397a6698c9878102a7dbb365e7c86c75cb05ef1
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 20:07:52 +0200

    :clock2: Add Stopwatch Benchmark

---
+++ src/Performance.NET7.Benchmarks/Diagnostics/Stopwatch/StopWatchBenchmarks.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace Performance.NET7.Benchmarks.Diagnostics.StopwatchPerformance;
+
+[RPlotExporter]
+[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0", baseline: true)]
+[MemoryDiagnoser(displayGenColumns: false)]
+[HideColumns(Column.Job, Column.StdDev, Column.Error, Column.RatioSD)]
+public class StopWatchBenchmarks
+{
+    [Benchmark(Baseline = true)]
+    public TimeSpan OldStopwatch()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        return sw.Elapsed;
+    }
+
+#if NET7_0
+    [Benchmark]
+    public TimeSpan NewStopwatch()
+    {
+        long timestamp = Stopwatch.GetTimestamp();
+        return Stopwatch.GetElapsedTime(timestamp);
+    }
+#endif
+}
