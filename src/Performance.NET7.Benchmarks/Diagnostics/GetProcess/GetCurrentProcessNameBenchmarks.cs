ecd0e7e7400cf46fb3f411e21e6f03f1b4637265
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 20:07:21 +0200

    :+1: Create GetCurrentProcessName

---
+++ src/Performance.NET7.Benchmarks/Diagnostics/GetProcess/GetCurrentProcessNameBenchmarks.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace Performance.NET7.Benchmarks.Diagnostics.GetProcess;
+
+[RPlotExporter]
+[SimpleJob(RuntimeMoniker.Net60, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 6.0")]
+[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0", baseline: true)]
+[MemoryDiagnoser(displayGenColumns: false)]
+[HideColumns(Column.Job, Column.StdDev, Column.Error, Column.RatioSD)]
+public class GetCurrentProcessNameBenchmarks
+{
+    [Benchmark]
+    public string GetCurrentProcessName() => Process.GetCurrentProcess().ProcessName;
+}
