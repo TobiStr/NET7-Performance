168b5f817a41ca96a96d4614828f11e8d7ba7c2c
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 20:09:23 +0200

    :paperclip: Add CacheBenchmarks

---
+++ src/Performance.NET7.Benchmarks/Serialization/Cache/CacheBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace Performance.NET7.Benchmarks.Serialization.Cache;
+
+[RPlotExporter]
+[SimpleJob(RuntimeMoniker.Net60, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 6.0")]
+[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0", baseline: true)]
+[MemoryDiagnoser(displayGenColumns: false)]
+[HideColumns(Column.Job, Column.StdDev, Column.Error, Column.RatioSD)]
+public class CacheBenchmarks
+{
+    private JsonSerializerOptions options = new JsonSerializerOptions();
+    private TestClass instance = new TestClass("Test");
+
+    [Benchmark(Baseline = true)]
+    public string Default() => JsonSerializer.Serialize(instance);
+
+    [Benchmark]
+    public string CachedOptions() => JsonSerializer.Serialize(instance, options);
+
+    [Benchmark]
+    public string NoCachedOptions() => JsonSerializer.Serialize(instance, new JsonSerializerOptions());
+
+    public record TestClass(string Test);
+}
