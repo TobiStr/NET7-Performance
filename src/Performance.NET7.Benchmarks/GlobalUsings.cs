59cb7921235f2c640757c86e751e972aa15e5feb
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 13:48:43 +0200

    :heavy_plus_sign: Add Columns

--- src/Performance.NET7.Benchmarks/GlobalUsings.cs
+++ src/Performance.NET7.Benchmarks/GlobalUsings.cs
@@ -1,4 +1,5 @@
 ﻿global using BenchmarkDotNet.Jobs;
 global using BenchmarkDotNet.Order;
+global using BenchmarkDotNet.Columns;
 global using BenchmarkDotNet.Attributes;
 global using BenchmarkDotNet.Mathematics;
