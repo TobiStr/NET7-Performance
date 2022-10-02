2b57d7050b294972d8a2b39a86ffa9b4c5382fa9
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 11:51:19 +0200

    :book: Create Benchmark Template

---
+++ src/Performance.NET7.Benchmarks/BenchmarkTemplate.cs
@@ -0,0 +1,22 @@
+﻿namespace Performance.NET7.Benchmarks;
+
+[ShortRunJob(RuntimeMoniker.Net60)]
+[ShortRunJob(RuntimeMoniker.Net70)]
+[RankColumn(NumeralSystem.Arabic)]
+[MemoryDiagnoser(displayGenColumns: false)]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class BenchmarkTemplate
+{
+    [Params(100)]
+    public int Count { get; set; }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+    }
+
+    [Benchmark]
+    public void Benchmark()
+    {
+    }
+}
