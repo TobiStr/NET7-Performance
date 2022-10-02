2dc17000a762dda290cfcd34b99d80b7c6c8c9cc
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 12:57:49 +0200

    :heavy_plus_sign: Add MinMax

---
+++ src/Performance.NET7.Benchmarks/LINQ/MinMax/MinMaxBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Columns;
+
+namespace Performance.NET7.Benchmarks.LINQ.MinMax;
+
+[RPlotExporter]
+[SimpleJob(RuntimeMoniker.Net60, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 6.0")]
+[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0")]
+[MemoryDiagnoser(displayGenColumns: false)]
+[HideColumns(Column.Job, Column.StdDev, Column.Error)]
+public class MinMaxBenchmarks
+{
+    [Params(1000)]
+    public int Length { get; set; }
+
+    private int[] arr;
+
+    [GlobalSetup]
+    public void GlobalSetup() => arr = Enumerable.Range(0, Length).ToArray();
+
+    [Benchmark]
+    public int Min() => arr.Min();
+
+    [Benchmark]
+    public int Max() => arr.Max();
+}
