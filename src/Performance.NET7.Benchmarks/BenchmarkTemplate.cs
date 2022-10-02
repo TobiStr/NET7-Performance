6016397fec6b83883953f491f47e3e17d6130d9a
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 13:14:37 +0200

    :100: Improve Template

--- src/Performance.NET7.Benchmarks/BenchmarkTemplate.cs
+++ src/Performance.NET7.Benchmarks/BenchmarkTemplate.cs
@@ -4,9 +4,9 @@ namespace Performance.NET7.Benchmarks;
 
 [RPlotExporter]
 [SimpleJob(RuntimeMoniker.Net60, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 6.0")]
-[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0")]
+[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0", baseline: true)]
 [MemoryDiagnoser(displayGenColumns: false)]
-[HideColumns(Column.Job, Column.StdDev, Column.Error)]
+[HideColumns(Column.Job, Column.StdDev, Column.Error, Column.RatioSD, Column.AllocRatio)]
 public class BenchmarkTemplate
 {
     [Params(100)]
@@ -17,7 +17,6 @@ public class BenchmarkTemplate
     {
     }
 
-    [Benchmark]
     public void Benchmark()
     {
     }
