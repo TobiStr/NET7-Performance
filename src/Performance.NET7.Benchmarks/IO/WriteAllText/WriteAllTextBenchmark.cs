78d3b67d6744a46ef57b6f1014d24887912b110a
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 14:58:30 +0200

    :heavy_plus_sign: Add WriteAllTextBenchmark

---
+++ src/Performance.NET7.Benchmarks/IO/WriteAllText/WriteAllTextBenchmark.cs
@@ -0,0 +1,26 @@
+﻿namespace Performance.NET7.Benchmarks.IO.WriteAllText;
+
+[RPlotExporter]
+[SimpleJob(RuntimeMoniker.Net60, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 6.0")]
+[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0", baseline: true)]
+[MemoryDiagnoser(displayGenColumns: false)]
+[HideColumns(Column.Job, Column.StdDev, Column.Error, Column.RatioSD, Column.AllocRatio)]
+public class WriteAllTextBenchmark
+{
+    private string path;
+
+    private string content;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        path = Path.GetRandomFileName();
+        content = string.Join(";", Enumerable.Range(0, 1000));
+    }
+
+    [GlobalCleanup]
+    public void Cleanup() => File.Delete(path);
+
+    [Benchmark]
+    public void WriteAllText() => File.WriteAllText(path, content);
+}
