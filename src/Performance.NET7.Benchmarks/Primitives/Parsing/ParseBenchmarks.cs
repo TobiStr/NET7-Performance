6f3ce487d3308ef6e07ff5a8c99fb4da72b85640
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 20:09:08 +0200

    :heavy_plus_sign: Add ParseBenchmarks

---
+++ src/Performance.NET7.Benchmarks/Primitives/Parsing/ParseBenchmarks.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace Performance.NET7.Benchmarks.Primitives.Parsing;
+
+[RPlotExporter]
+[SimpleJob(RuntimeMoniker.Net60, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 6.0")]
+[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0", baseline: true)]
+[MemoryDiagnoser(displayGenColumns: false)]
+[HideColumns(Column.Job, Column.StdDev, Column.Error, Column.RatioSD, Column.AllocRatio)]
+public class ParseBenchmarks
+{
+    private string bigIntString = string.Concat(Enumerable.Repeat("123456789", 100000));
+
+    [Benchmark]
+    public BigInteger ParseBigInt() => BigInteger.Parse(bigIntString);
+
+    [Benchmark]
+    public bool ParseBool() => bool.TryParse("True", out _);
+}
