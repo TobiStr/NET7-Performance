22bbc830dd19361fc5d599af8f446df8a0298eee
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-02 14:58:14 +0200

    :heavy_plus_sign: Add DirectoryMoveBenchmark

---
+++ src/Performance.NET7.Benchmarks/IO/DirectoryMove/DirectoryMoveBenchmarks.cs
@@ -0,0 +1,28 @@
+﻿namespace Performance.NET7.Benchmarks.IO.DirectoryMove;
+
+[RPlotExporter]
+[SimpleJob(RuntimeMoniker.Net60, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 6.0")]
+[SimpleJob(RuntimeMoniker.Net70, launchCount: 1, warmupCount: 3, targetCount: 5, invocationCount: -1, id: "NET 7.0", baseline: true)]
+[MemoryDiagnoser(displayGenColumns: false)]
+[HideColumns(Column.Job, Column.StdDev, Column.Error, Column.RatioSD, Column.AllocRatio)]
+public class DirectoryMoveBenchmarks
+{
+    private string path1;
+
+    private string path2;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        path1 = Path.GetTempFileName();
+        path2 = Path.GetTempFileName();
+        File.Delete(path2);
+        Directory.CreateDirectory(path1);
+    }
+
+    [Benchmark]
+    public void Move()
+    {
+        Directory.Move(path1, path2);
+    }
+}
